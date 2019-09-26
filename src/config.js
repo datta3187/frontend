070ede0b0e62d406c7bdb63141d2c22fbe52134c
Jag <jag@antech.in>
2019-09-26 12:31:57 +0530

    phone and setting page

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 // Set captchaPolicy to disabled if not required. Check barong recaptcha policy.
 
 export default {
-  apiUrl: "http://bbff69fd.ngrok.io/api/v2/barong",
-  peatioUrl: "http://bbff69fd.ngrok.io/api/v2/peatio",
+  apiUrl: "http://21e73c13.ngrok.io/api/v2/barong",
+  peatioUrl: "http://21e73c13.ngrok.io/api/v2/peatio",
   captchaPolicy: true,
   recatpchaSiteKey: "6Ldk47kUAAAAADVE3xKJGnt2g4xuRZT6ds_ojc6i"
 };
+
