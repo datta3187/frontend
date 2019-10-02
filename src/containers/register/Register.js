f3c7d90da68eb23700daee4637f93de4fb5c8d62
anuj <anuj.dhiman@antechindia.com>
2019-10-02 11:42:35 +0530

    pulled latest code at local

--- src/containers/register/Register.js
+++ src/containers/register/Register.js
@@ -140,7 +140,7 @@ class Register extends Component {
                 })
                 .catch(error => {
                     this.setState({ loading: false });
-                    if (config.captchaPolicy) {
+                    if(config.captchaPolicy){
                         this.recaptcha.reset();
                     }
                     CustomError.handle(error)
