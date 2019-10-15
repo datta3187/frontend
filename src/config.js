978fb3a7932c42ef0533a8d8308e5693c7dec641
Anuj <anuj.dhiman@antechindia.com>
2019-10-15 18:05:21 +0530

    changes referral ID

--- src/config.js
+++ src/config.js
@@ -13,6 +13,6 @@ export default {
   webSocketUrl: 'ws://stage.tradence.io/api/v2/ranger/public/?stream=',
   captchaPolicy: false,
   recatpchaSiteKey: "6Ldk47kUAAAAADVE3xKJGnt2g4xuRZT6ds_ojc6i",
-  referralId: 'EX862AB0BC21'
+  referralId: 'EXC5EF234078'
 
 };
