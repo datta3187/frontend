b10763ab509e30235d7ed3daeda33e9c38c66806
Anuj <anuj.dhiman@antechindia.com>
2019-10-15 17:43:20 +0530

    changes for stage url in config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,18 @@
 export default {
-  baseUrl: 'http://www.app.local/api/v2',
-  apiUrl: "http://www.app.local/api/v2/barong",
-  peatioUrl: "http://www.app.local/api/v2/peatio",
-  webSocketUrl: 'ws://www.app.local/api/v2/ranger/public/?stream=',
+  // baseUrl: 'http://www.app.local/api/v2',
+  // apiUrl: "http://www.app.local/api/v2/barong",
+  // peatioUrl: "http://www.app.local/api/v2/peatio",
+  // webSocketUrl: 'ws://www.app.local/api/v2/ranger/public/?stream=',
+  // captchaPolicy: false,
+  // recatpchaSiteKey: "6Ldk47kUAAAAADVE3xKJGnt2g4xuRZT6ds_ojc6i",
+  // referralId: 'EX862AB0BC21'
+
+  baseUrl: 'http://stage.tradence.io/api/v2',
+  apiUrl: "http://stage.tradence.io/api/v2/barong",
+  peatioUrl: "http://stage.tradence.io/api/v2/peatio",
+  webSocketUrl: 'ws://stage.tradence.io/api/v2/ranger/public/?stream=',
   captchaPolicy: false,
   recatpchaSiteKey: "6Ldk47kUAAAAADVE3xKJGnt2g4xuRZT6ds_ojc6i",
   referralId: 'EX862AB0BC21'
+
 };
