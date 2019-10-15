b2d2761a887a6091e71f1dfd957f53c1d9a9c35a
Anuj <anuj.dhiman@antechindia.com>
2019-10-15 17:55:47 +0530

    moved bootstarp css into exchange

--- src/containers/trade/Exchange.js
+++ src/containers/trade/Exchange.js
@@ -12,6 +12,7 @@ import MyOrder from './MyOrder'
 import OrderHistory from './OrderHistory'
 // import PeatioSocket from '../../components/PeatioSocket'
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import './css/exchange.scss'
