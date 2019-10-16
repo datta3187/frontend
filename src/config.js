5521bb3d614d8f0020b6f519ad0790cb498796d4
Anuj <anuj.dhiman@antechindia.com>
2019-10-16 17:01:08 +0530

    changes in config file for stage/dev separate

--- src/config.js
+++ src/config.js
@@ -1,18 +1,9 @@
 export default {
-  // baseUrl: 'http://www.app.local/api/v2',
-  // apiUrl: "http://www.app.local/api/v2/barong",
-  // peatioUrl: "http://www.app.local/api/v2/peatio",
-  // webSocketUrl: 'ws://www.app.local/api/v2/ranger/public/?stream=',
-  // captchaPolicy: false,
-  // recatpchaSiteKey: "6Ldk47kUAAAAADVE3xKJGnt2g4xuRZT6ds_ojc6i",
-  // referralId: 'EX862AB0BC21'
-
-  baseUrl: 'http://stage.tradence.io/api/v2',
-  apiUrl: "http://stage.tradence.io/api/v2/barong",
-  peatioUrl: "http://stage.tradence.io/api/v2/peatio",
-  webSocketUrl: 'ws://stage.tradence.io/api/v2/ranger/public/?stream=',
+  baseUrl: 'http://www.app.local/api/v2',
+  apiUrl: "http://www.app.local/api/v2/barong",
+  peatioUrl: "http://www.app.local/api/v2/peatio",
+  webSocketUrl: 'ws://www.app.local/api/v2/ranger/public/?stream=',
   captchaPolicy: false,
   recatpchaSiteKey: "6Ldk47kUAAAAADVE3xKJGnt2g4xuRZT6ds_ojc6i",
-  referralId: 'EXC5EF234078'
-
+  referralId: 'EX862AB0BC21'
 };
