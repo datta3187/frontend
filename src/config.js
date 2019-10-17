e129c1859b226a6081f68f7e78f6d1dc84a7c399
Anuj <anuj.dhiman@antechindia.com>
2019-10-17 14:56:02 +0530

    changes for stage merge

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  baseUrl: 'http://www.app.local/api/v2',
-  apiUrl: "http://www.app.local/api/v2/barong",
-  peatioUrl: "http://www.app.local/api/v2/peatio",
-  webSocketUrl: 'ws://www.app.local/api/v2/ranger/public/?stream=',
-  captchaPolicy: false,
-  recatpchaSiteKey: "6Ldk47kUAAAAADVE3xKJGnt2g4xuRZT6ds_ojc6i",
-  referralId: 'EX862AB0BC21'
-};
