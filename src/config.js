8b77170ad1ef355b7fa9add8e1dfc4c957d33973
Anuj <anuj.dhiman@antechindia.com>
2019-10-22 20:51:31 +0530

    Merged all steps of loggedIn with Saga

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+  baseUrl: 'http://stage.tradence.io/api/v2',
+  apiUrl: "http://stage.tradence.io/api/v2/barong",
+  peatioUrl: "http://stage.tradence.io/api/v2/peatio",
+  webSocketUrl: 'ws://stage.tradence.io/api/v2/ranger/public/?stream=',
+  captchaPolicy: false,
+  recatpchaSiteKey: "6Ldk47kUAAAAADVE3xKJGnt2g4xuRZT6ds_ojc6i",
+  referralId: 'EXC5EF234078'
+
+};
