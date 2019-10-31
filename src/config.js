6d71f77961d3f0f3142fae74ec4ea1489e651de9
Anuj <anuj.dhiman@antechindia.com>
2019-10-31 19:35:53 +0530

    changes in config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+  baseUrl: 'http://promotion.tradence.io/api/v2',
+  apiUrl: "http://promotion.tradence.io/api/v2/barong",
+  peatioUrl: "http://promotion.tradence.io/api/v2/peatio",
+  webSocketUrl: 'ws://promotion.tradence.io/api/v2/ranger/public/?stream=',
+  captchaPolicy: false,
+  recatpchaSiteKey: "6Ldk47kUAAAAADVE3xKJGnt2g4xuRZT6ds_ojc6i",
+  referralId: 'EX68D3519624',
+  domainUrl: 'http://promotion.tradence.io'
+};
