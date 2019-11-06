4db289834fb89e903e469f171ab38803dc22e813
Anuj <anuj.dhiman@antechindia.com>
2019-11-06 15:54:55 +0530

    changes referral ID on prod

--- src/config.js
+++ src/config.js
@@ -1,10 +1,11 @@
 export default {
-  baseUrl: 'http://promotion.tradence.io/api/v2',
-  apiUrl: "http://promotion.tradence.io/api/v2/barong",
-  peatioUrl: "http://promotion.tradence.io/api/v2/peatio",
-  webSocketUrl: 'ws://promotion.tradence.io/api/v2/ranger/public/?stream=',
+  baseUrl: 'https://promotion.tradence.io/api/v2',
+  apiUrl: "https://promotion.tradence.io/api/v2/barong",
+  peatioUrl: "https://promotion.tradence.io/api/v2/peatio",
+  webSocketUrl: 'wss://promotion.tradence.io/api/v2/ranger/public/?stream=',
   captchaPolicy: false,
   recatpchaSiteKey: "6Ldk47kUAAAAADVE3xKJGnt2g4xuRZT6ds_ojc6i",
-  referralId: 'EX68D3519624',
-  domainUrl: 'http://promotion.tradence.io'
+  referralId: 'EXDCD65FC6AD',
+  domainUrl: 'https://promotion.tradence.io'
 };
+
